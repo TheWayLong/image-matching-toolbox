8b15157a8b1bcb0b7fccea2b8645b6b37f8b64a8
TheWayLong <145312286+TheWayLong@users.noreply.github.com>
2023-11-03 22:24:46 +0800

    Update download.sh

--- pretrained/download.sh
+++ pretrained/download.sh
@@ -10,14 +10,14 @@ wget https://dsmn.ml/files/d2-net/d2_tf.pth -O d2net/d2_tf.pth
 wget https://dsmn.ml/files/d2-net/d2_tf_no_phototourism.pth -O d2net/d2_tf_no_phototourism.pth
 
 # R2D2 Symbolic  links
-ln -s ../third_party/r2d2/models  r2d2
+ln -sf ../third_party/r2d2/models  r2d2
 
 # SparseNCNet
 mkdir -p sparsencnet
 wget https://www.di.ens.fr/willow/research/sparse-ncnet/models/sparsencnet_k10.pth.tar  -O sparsencnet/sparsencnet_k10.pth.tar 
 
 # Patch2Pix Symbolic links
-ln -s ../third_party/patch2pix/pretrained patch2pix
+ln -sf ../third_party/patch2pix/pretrained patch2pix
 cd patch2pix
 bash download.sh
 cd ..
@@ -26,6 +26,7 @@ cd ..
 mkdir -p loftr
 gdown --id 1M-VD35-qdB5Iw-AtbDBCKC7hPolFW9UY -O loftr/outdoor_ds.ckpt
 gdown --id 19s3QvcCWQ6g-N1PrYlDCg-2mOJZ3kkgS -O loftr/indoor_ds_new.ckpt
+gdown --id 1kW2bQejjMlmE7FGberHrubXpE_ttX2LB -O loftr/indoor_ot.ckpt
 
 # COTR
 mkdir -p cotr
@@ -37,4 +38,4 @@ rm default.zip
 cd ..
 
 # ASpanFormer Symbolic  links
-ln -s ../third_party/aspanformer/weights  aspanformer
+ln -sf ../third_party/aspanformer/weights  aspanformer
